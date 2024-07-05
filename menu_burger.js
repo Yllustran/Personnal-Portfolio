39adea44d85b1282b9242886e067e7b3910ddcea
Yllustran <sullivantra@gmail.com>
2024-07-05 20:20:13 +0200

    - Correction bug menu_burger.js - Ajout Projet Section - Netoyage du css

--- menu_burger.js
+++ menu_burger.js
@@ -1,9 +1,17 @@
+// Attendre que la page soit complètement chargée
 document.addEventListener('DOMContentLoaded', function() {
+    // Trouver l'élément avec la classe 'header_burger'
     const burger = document.querySelector('.header_burger');
+    // Trouver l'élément avec la classe 'header_right'
     const headerRight = document.querySelector('.header_right');
+    // Trouver l'élément avec la classe 'li_github'
     const gitHub = document.querySelector('.li_github');
 
-        burger.addEventListener('click', function() {
+    // Ajouter un écouteur d'événement au clic sur 'burger'
+    burger.addEventListener('click', function() {
+        // Ajouter ou enlever la classe 'active' sur 'burger'
         burger.classList.toggle('active');
-        });
+            // Ajouter ou enlever la classe 'active' sur 'burger'
+            headerRight.classList.toggle('active');
+    });
 });
